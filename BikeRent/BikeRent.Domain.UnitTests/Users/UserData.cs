6f2b4b1fea2aa8c7b22aa36664a73ecae53ec294
Illia Nankov <iliya2002pl.ru@gmail.com>
2024-06-17 21:12:51 +0300

    Unit tests project added.

---
+++ BikeRent/BikeRent.Domain.UnitTests/Users/UserData.cs
@@ -0,0 +1,11 @@
+﻿using BikeRent.Domain.Users;
+
+namespace BikeRent.Domain.UnitTests.Users
+{
+    internal static class UserData
+    {
+        public static readonly FirstName FirstName = new FirstName("John");
+        public static readonly LastName LastName = new LastName("Doe");
+        public static readonly Email Email = new Email("john.doe@example.com");
+    }
+}
---
+++ BikeRent/BikeRent.Domain.UnitTests/Users/UserTests.cs
@@ -0,0 +1,18 @@
+﻿using BikeRent.Domain.Users;
+
+namespace BikeRent.Domain.UnitTests.Users
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Create_Should_SetProperties()
+        {
+            var user = User.CreateUser(UserData.FirstName, UserData.LastName, UserData.Email);
+
+            Assert.NotNull(user);
+            Assert.Equal(UserData.FirstName, user.FirstName);
+            Assert.Equal(UserData.LastName, user.LastName);
+            Assert.Equal(UserData.Email, user.Email);
+        }
+    }
+}
